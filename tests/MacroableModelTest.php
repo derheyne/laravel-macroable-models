6d4883a18b675a0436b2a35b08de124b1b599efb
Daniel Heyne <derheyne@sylab.io>
2021-06-16 23:29:21 +0200

    Add additional tests to ensure native model functions are still working.
    And refactor existing tests a bit.

--- tests/MacroableModelTest.php
+++ tests/MacroableModelTest.php
@@ -39,59 +39,96 @@ class MacroableModelTest extends TestCase
     /** @test */
     public function a_macroable_model_can_have_an_accessor_macro(): void
     {
-        ModelWithMacros::macro('getTestAttributeAttribute', function ($value) {
+        ModelWithMacros::macro('getMacroAccessorAttribute', function ($value) {
             return 'modified: ' . $value;
         });
 
-        $model = new ModelWithMacros([
-            'test_attribute' => 'value',
-        ]);
+        $model = new ModelWithMacros();
+        $model->macro_accessor = 'macro accessor value';
 
-        self::assertEquals('modified: value', $model->test_attribute);
+        self::assertEquals('modified: macro accessor value', $model->macro_accessor);
     }
 
     /** @test */
     public function a_macroable_model_can_have_a_computed_accessor_macro(): void
     {
-        ModelWithMacros::macro('getCustomAttributeAttribute', function () {
+        ModelWithMacros::macro('getMacroComputedAccessorAttribute', function () {
             /** @var $this ModelWithMacros */
             return $this::class;
         });
 
         $model = new ModelWithMacros();
 
-        self::assertEquals(ModelWithMacros::class, $model->custom_attribute);
+        self::assertEquals(ModelWithMacros::class, $model->macro_computed_accessor);
     }
 
     /** @test */
     public function a_macroable_model_can_have_a_mutator_macro(): void
     {
-        ModelWithMacros::macro('setCustomAttributeAttribute', function ($value) {
+        ModelWithMacros::macro('setMacroMutatorAttribute', function ($value) {
             /** @mixin ModelWithMacros */
-            $this->attributes['custom_attribute'] = 'modified: ' . $value;
+            $this->attributes['macro_mutator'] = 'modified: ' . $value;
         });
 
         $model = new ModelWithMacros();
-        $model->custom_attribute = 'value';
+        $model->macro_mutator = 'macro mutator value';
 
-        self::assertEquals('modified: value', $model->custom_attribute);
+        self::assertEquals('modified: macro mutator value', $model->macro_mutator);
     }
 
     /** @test */
     public function a_macroable_model_can_have_a_local_scope_macro(): void
     {
-        ModelWithMacros::macro('scopeTest', function (Builder $query) {
-            return $query->where('test_column', 'hello world');
+        ModelWithMacros::macro('scopeMacroScope', function (Builder $query) {
+            return $query->where('macro_scope_column', 'hello world');
         });
 
         $model = new ModelWithMacros();
-        $query = $model->newQuery()->test();
+        $query = $model->newQuery()->macroScope();
 
         /** @noinspection SqlNoDataSourceInspection */
         self::assertEquals(
-            'select * from `table` where `test_column` = ?',
+            'select * from `table` where `macro_scope_column` = ?',
             $query->toSql()
         );
+    }
+
+    /** @test */
+    public function a_macroable_model_can_have_native_local_scopes(): void
+    {
+        $model = new ModelWithMacros();
+        $query = $model->newQuery()->localScope();
+
+        self::assertEquals(
+            'select * from `table` where `local_scope_column` = ?',
+            $query->toSql()
+        );
+    }
+
+    /** @test */
+    public function a_macroable_model_can_have_native_accessors(): void
+    {
+        $model = new ModelWithMacros();
+        $model->native_accessor = 'native accessor value';
+
+        self::assertEquals('modified: native accessor value', $model->native_accessor);
+    }
+
+    /** @test */
+    public function a_macroable_model_can_have_native_computed_accessors(): void
+    {
+        $model = new ModelWithMacros();
+
+        self::assertEquals($model::class, $model->native_computed_accessor);
+    }
+
+    /** @test */
+    public function a_macroable_model_can_have_native_mutators(): void
+    {
+        $model = new ModelWithMacros();
+        $model->native_mutator = 'native mutator value';
+
+        self::assertEquals('modified: native mutator value', $model->native_mutator);
     }
 
     public function setUp(): void
@@ -104,7 +141,7 @@ class MacroableModelTest extends TestCase
     public function tearDown(): void
     {
         // Reset static $macros property within ModelWithMacro class.
-        $reflection = new ReflectionClass(new ModelWithMacros);
+        $reflection = new ReflectionClass(new ModelWithMacros());
         $instance = $reflection->getProperty('macros');
         $instance->setAccessible(true);
         $instance->setValue([]);
@@ -116,12 +153,28 @@ class ModelWithMacros extends MacroableModel
 {
     protected $table = 'table';
 
-    protected $fillable = [
-        'test_attribute',
-    ];
-
     public static function hasMacros(): bool
     {
         return count(self::$macros) > 0;
+    }
+
+    public function scopeLocalScope($query)
+    {
+        return $query->where('local_scope_column', 'value');
+    }
+
+    public function getNativeAccessorAttribute($value)
+    {
+        return 'modified: ' . $value;
+    }
+
+    public function getNativeComputedAccessorAttribute()
+    {
+        return $this::class;
+    }
+
+    public function setNativeMutatorAttribute($value)
+    {
+        $this->attributes['native_mutator'] = 'modified: ' . $value;
     }
 }
